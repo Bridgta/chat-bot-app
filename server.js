e04d2a6879f62ad9ec32d39dec8d5b85e447b4e7
Bridget Asser <bridgetasser@gmail.com>
2020-03-18 22:56:48 -0400

    setting up web socket func

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("hello from node script");
+const express = require("express");
+const http = require("http");
+const app = express();
+const io = require("socket.io")(server);
+
+const server = http.Server(app);
+
+app.use(express.static("client"));
+
+io.on("connection", function(socket) {
+    socket.on("message", function(msg) {
+        io.emit("message", msg);
+    });
+});
+
+server.listen(8080, function() {
+    console.log("Chat server running");
+});
